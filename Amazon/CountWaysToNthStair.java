23bc2b430182eff29689ed54171e8ffac7f2e799
codificador69 <65847810+codificador69@users.noreply.github.com>
2022-01-09 03:56:01 +0530

    Day2 update

--- Amazon/CountWaysToNthStair.java
+++ Amazon/CountWaysToNthStair.java
@@ -1,5 +1,60 @@
-package Amazon;
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
 
-public class CountWaysToNthStair {
 
+class GFG {
+	public static void main (String[] args) {
+		
+		//taking input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking total testcases
+		int t = sc.nextInt();
+		
+		while(t-- > 0){
+		    
+		    //taking total number of stairs
+		    int m = sc.nextInt();
+		    
+		    //creating an object of class DynamicProgramming
+		    Solution obj = new Solution();
+		    
+		    //calling method of class countWays()
+		    //of class DynamicProgramming
+		    System.out.println(obj.countWays(m));
+		    
+		}
+		
+	}
 }
+// } Driver Code Ends
+
+
+
+class Solution
+{
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    Long countWays(int m)
+    {
+        
+        return (long)((m/2) +1);
+    }    
+}
+
+/*
+There are N stairs, and a person standing at the bottom wants to reach the top. The person can climb either 1 stair or 2 stairs at a time. Count the number of ways, the person can reach the top (order does not matter).
+Note: Order does not matter means for n=4 {1 2 1},{2 1 1},{1 1 2} are considered same.
+
+Example 1:
+
+Input:
+N = 4
+Output: 3
+Explanation: You can reach 4th stair in
+3 ways.
+3 possible ways are:
+1, 1, 1, 1
+1, 1, 2
+2, 2*/
